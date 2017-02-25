8efab8161391babb444c81173daa3d0d6ae7a7f9
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2017-02-25 16:50:57 +0100

    slight change

--- feature_extraction.py
+++ feature_extraction.py
@@ -65,14 +65,8 @@ def group_by_recipient(dic):
 
     
 if __name__ == "__main__":
-#    data_info = read_data_info(nrows=50)
-#
-#    corpus, mids, recipients = build_corpus(data_info)
-#    bag_of_words, _ = get_bag_words(corpus, mids, recipients)
-    
-#    recipient_dict = group_by_recipient(bag_of_words)
-    print(type(bag_of_words))
-    
-    
-
-#    get_word2vec = get_word2vec(data_info)
+    data_info = read_data_info()
+    corpus, mids, recipients = build_corpus(data_info)
+    bag_of_words, _ = get_bag_words(corpus, mids, recipients)
+    recipient_dict = group_by_recipient(bag_of_words)
+    #get_word2vec = get_word2vec(data_info)
