14698044cba2965c1bf3466c0510c4959b4ec8a8
LIU Zhengying <zhengying.liu@polytechnique.edu>
2017-02-26 01:37:24 +0100

    nearest message baseline
    give a score of 0.01044
    lol

--- baseline_zhengying.py
+++ baseline_zhengying.py
@@ -10,14 +10,10 @@ import pandas as pd
 
 def get_dataframes():
     path_to_data = "input/"
-    training = pd.read_csv(path_to_data + 'training_set.csv', sep=',', 
-                           index_col="sender")
-    training_info = pd.read_csv(path_to_data + 'training_info.csv', sep=',',
-                                index_col="mid")
-    test = pd.read_csv(path_to_data + 'test_set.csv', sep=',',
-                       index_col="sender")
-    test_info = pd.read_csv(path_to_data + 'test_info.csv', sep=',',
-                            index_col="mid")
+    training = pd.read_csv(path_to_data + 'training_set.csv', sep=',')
+    training_info = pd.read_csv(path_to_data + 'training_info.csv', sep=',')
+    test = pd.read_csv(path_to_data + 'test_set.csv', sep=',')
+    test_info = pd.read_csv(path_to_data + 'test_info.csv', sep=',')
     
     # list of mids in training, test
     print("Constructing list_of_mids...")
@@ -41,9 +37,10 @@ def get_dataframes():
         list_of_mids = row["list_of_mids"]
         res = dict()
         for mid in list_of_mids:
-            recipients = training_info.loc[mid]["list_of_recipients"]
+            idx = np.where(training_info["mid"] == mid)[0][0]
+            recipients = training_info.loc[idx]["list_of_recipients"]
             # add sender to training_info
-            training_info["sender"][mid] = row.name
+            training_info.loc[idx, "sender"] = row.sender
             for rec in recipients:
                 if rec not in res:
                     res[rec] = 0
@@ -58,11 +55,32 @@ def get_dataframes():
     for index, row in test.iterrows():
         list_of_mids = row["list_of_mids"]
         for mid in list_of_mids:
-            test_info["sender"][mid] = index
+            idx = np.where(test_info["mid"] == mid)[0][0]
+            test_info.loc[idx, "sender"] = row.sender
     
     return training, training_info, test, test_info
- 
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+def predict_by_nearest_message(training_info, test_info, path="pred.txt"):
+    count_vect = CountVectorizer(stop_words='english')
+    corpus = training_info.body
+    X_train = count_vect.fit_transform(corpus)
+    X_test = count_vect.transform(test_info.body)
+    similarity = X_test.dot(X_train.T).toarray()
+    indexes_of_nearest_message = similarity.argmax(axis=1)
+    # for each row of test_info, return recipients of the nearest message 
+    # in training_info
+    func = lambda row: training_info.loc[indexes_of_nearest_message[row.name]]\
+                                        ["recipients"]
+    test_info["recipients"] = test_info.apply(func, axis=1)
+    pred = test_info[["mid","recipients"]]
+    pred.to_csv(path, index=False)
+    return pred, similarity, count_vect
+    
 
 if __name__ == "__main__":
-    training, training_info, test, test_info = get_dataframes()
-    print(test_info)
+#    training, training_info, test, test_info = get_dataframes()
+    pred, similarity, count_vect = predict_by_nearest_message(training_info, test_info)
+    print(pred)
