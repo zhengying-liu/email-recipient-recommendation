02427bbf4015d488b2f1a9449e55370476fd0e89
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2017-03-08 17:05:59 +0100

    evaluation --still not working

---
+++ evaluation.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 09:44:23 2017
+
+@author: salma
+"""
+
+import numpy as np
+
+def apk(actual, predicted, k=10):
+    """
+    Computes the average precision at k.
+
+    This function computes the average prescision at k between two lists of
+    items.
+
+    Parameters
+    ----------
+    actual : list
+             A list of elements that are to be predicted (order doesn't matter)
+    predicted : list
+                A list of predicted elements (order does matter)
+    k : int, optional
+        The maximum number of predicted elements
+
+    Returns
+    -------
+    score : double
+            The average precision at k over the input lists
+
+    """
+    if len(predicted)>k:
+        predicted = predicted[:k]
+
+    score = 0.0
+    num_hits = 0.0
+
+    for i,p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i+1.0)
+
+    if not actual:
+        return 0.0
+
+    return score / min(len(actual), k)
+
+def mapk(actual, predicted, k=10):
+    """
+    Computes the mean average precision at k.
+
+    This function computes the mean average prescision at k between two lists
+    of lists of items.
+
+    Parameters
+    ----------
+    actual : list
+             A list of lists of elements that are to be predicted 
+             (order doesn't matter in the lists)
+    predicted : list
+                A list of lists of predicted elements
+                (order matters in the lists)
+    k : int, optional
+        The maximum number of predicted elements
+
+    Returns
+    -------
+    score : double
+            The mean average precision at k over the input lists
+
+    """
+    return np.mean([apk(a,p,k) for a,p in zip(actual, predicted)])
+    
+def split_train_test(training_info, pr_train=0.8):
+    n_train = int(len(training_info) * pr_train)
+    indices = list(training_info.index)
+    train_indices = np.random.choice(indices, size=n_train, replace=False)
+    test_indices = [x for x in indices if x not in train_indices]
+    training_info_t = training_info[training_info['mid'].isin(train_indices)]
+    training_info_v = training_info[training_info['mid'].isin(test_indices)]
+    return training_info_t, training_info_v
+    
+
+def get_validation_score(training_info_t, prediction_df, train_idx, test_idx):
+    training_info_t = training_info_t.sort(column='mid', axis=0)
+    prediction_df = prediction_df.sort(column='mid', axis=0)
+    list_list_recipients_true = list(training_info_t.list_of_recipients.values)
+    list_list_recipients_pred = list(prediction_df['recipients'].values)
+    return mapk(list_list_recipients_true, list_list_recipients_pred)
+        
+    
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from nltk import word_tokenize
 from tqdm import tqdm
 import pandas as pd
-
+import numpy as np
 import string
 import re
 import itertools
@@ -90,6 +90,92 @@ def read_data_info(filename="input/training_info.csv", nrows=None):
             info_dict[mid]['recipients'] = recipients
 
     return info_dict
+    
+def get_dataframes():
+    """
+    Construct four pd.DataFrame: training, training_info, test, test_info
+    
+    columns in training: ['sender', 'mids', 'list_of_mids', 'address_book']
+    columns in training_info: ['mid', 'date', 'body', 'recipients', 
+                               'list_of_recipients', 'sender']
+    columns in test: ['sender', 'mids', 'list_of_mids', 'address_book']
+    columns in test_info: ['mid', 'date', 'body', 'sender']
+    
+    """
+    path_to_data = "input/"
+    training = pd.read_csv(path_to_data + 'training_set.csv', sep=',')
+    training_info = pd.read_csv(path_to_data + 'training_info.csv', sep=',')
+    test = pd.read_csv(path_to_data + 'test_set.csv', sep=',')
+    test_info = pd.read_csv(path_to_data + 'test_info.csv', sep=',')
+    
+    # list of mids in training, test
+    print("Constructing list_of_mids...")
+    func = lambda row: list(map(int, row.mids.split(' ')))
+    training["list_of_mids"] = training.apply(func, axis=1)
+    test["list_of_mids"] = test.apply(func, axis=1)
+    
+    # list of recipients in training_info
+    print("Constructing list_of_recipients...")
+    func = lambda row: [rec for rec in row.recipients.split(' ') if '@' in rec]
+    training_info["list_of_recipients"] = training_info.apply(func, axis=1)
+    
+    # create an empty column for sender in training_info, test_info
+    func = lambda row: ""
+    training_info["sender"] = training_info.apply(func,axis=1)
+    test_info["sender"] = test_info.apply(func,axis=1)
+
+    # address book in training, test
+    print("Constructing address book...")
+    def count_contacted_recipients(row):
+        list_of_mids = row["list_of_mids"]
+        res = dict()
+        for mid in list_of_mids:
+            idx = np.where(training_info["mid"] == mid)[0][0]
+            recipients = training_info.loc[idx]["list_of_recipients"]
+            # add sender to training_info
+            training_info.loc[idx, "sender"] = row.name
+            for rec in recipients:
+                if rec not in res:
+                    res[rec] = 1
+                else:
+                    res[rec] += 1
+        return res
+    training["address_book"]= training.apply(count_contacted_recipients,axis=1)
+    test["address_book"] = training["address_book"].copy()
+    
+    # add sender to test_info
+    print("Add sender to test_info...")
+    for index, row in test.iterrows():
+        list_of_mids = row["list_of_mids"]
+        for mid in list_of_mids:
+            idx = np.where(test_info["mid"] == mid)[0][0]
+            test_info.loc[idx, "sender"] = row.name
+    
+    return training, training_info, test, test_info
+
+def received_mails_of_each_recipient_by_index(training_info_train):
+    """
+    Return a pd.DataFrame: mails_of_each_recipient
+    index of mails_of_each_recipient: recipient
+    columns in mails_of_each_recipient: ['list_of_messages_by_index',
+                                         'number_of_received_messages']
+    """
+    print("Constructing received_mids_for_each_recipient...")
+    recipient_mids_dict = dict()
+    for index, row in training_info_train.iterrows():
+        list_of_recipients = row["list_of_recipients"]
+        for recipient in list_of_recipients:
+            if recipient in recipient_mids_dict:
+                recipient_mids_dict[recipient][0].append(index)
+            else:
+                recipient_mids_dict[recipient] = [[index]]
+    mails_of_each_recipient = pd.DataFrame(recipient_mids_dict).T
+    mails_of_each_recipient.columns = ['list_of_messages_by_index']
+    mails_of_each_recipient['number_of_received_messages'] =\
+                           mails_of_each_recipient.apply(lambda row: 
+                               len(row['list_of_messages_by_index']),axis=1)
+                        
+    return mails_of_each_recipient
 
 if __name__ == "__main__":
-    print(read_data_info())
+   pass
